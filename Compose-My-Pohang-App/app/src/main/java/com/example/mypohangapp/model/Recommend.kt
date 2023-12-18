60c9fff36c9bae69827cd05f792f346b362fc9d7
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-18 18:17:23 +0900

    Add source resource.

--- Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/model/Recommend.kt
+++ Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/model/Recommend.kt
@@ -2,12 +2,13 @@ package com.example.mypohangapp.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.example.mypohangapp.R
 
 data class Recommend(
     val categoryType: CategoryType,
     @StringRes val name: Int,
     @StringRes val location: Int,
     @StringRes val information: Int,
-    @StringRes val source: Int,
+    @StringRes val source: Int = R.string.recommend_source,
     @DrawableRes val picture: Int
 )
