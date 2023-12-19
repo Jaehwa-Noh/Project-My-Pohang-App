37a4d1398b2867461a39065002ebf65cf2c18071
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-19 10:49:30 +0900

    Gradle update. - navVersion 2.7.6

--- Compose-My-Pohang-App/app/build.gradle.kts
+++ Compose-My-Pohang-App/app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    val navVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-compose:$navVersion")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
