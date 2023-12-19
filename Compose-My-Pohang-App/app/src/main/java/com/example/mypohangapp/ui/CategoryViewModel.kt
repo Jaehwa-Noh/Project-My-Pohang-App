f18683859aa7743a0dd905728c83fa82e24297a9
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-19 14:45:47 +0900

    Create CategoryViewModel.

---
+++ Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/ui/CategoryViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.mypohangapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.mypohangapp.model.Category
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class CategoryViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(CategoryUiState())
+    val uiState: StateFlow<CategoryUiState> = _uiState.asStateFlow()
+
+    fun selectCategory(category: Category) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                category = category
+            )
+        }
+    }
+}
+
+data class CategoryUiState(
+    val category: Category? = null
+)
