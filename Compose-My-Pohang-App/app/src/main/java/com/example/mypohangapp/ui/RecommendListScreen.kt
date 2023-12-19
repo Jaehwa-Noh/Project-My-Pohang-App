311f78a49b1dcdbbc555d32c1012a9bf90cb1660
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-19 16:23:26 +0900

    Create RecommendListSCreen.

--- Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/ui/RecommendListScreen.kt
+++ Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/ui/RecommendListScreen.kt
@@ -1,20 +1,83 @@
 package com.example.mypohangapp.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mypohangapp.data.CategoryAndRecommendRepository
+import com.example.mypohangapp.model.Recommend
+import com.example.mypohangapp.ui.theme.MyPohangAppTheme
 
 @Composable
-fun RecommendListScreen(modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(1) {
-            RecommendListItem()
+fun RecommendListScreen(
+    modifier: Modifier = Modifier,
+    recommendList: List<Recommend>,
+    onRecommendClick: (Recommend) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        items(recommendList) { recommend ->
+            RecommendListItem(
+                recommend = recommend,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .clickable {
+                        onRecommendClick(recommend)
+                    }
+            )
         }
 
     }
 }
 
 @Composable
-fun RecommendListItem(modifier: Modifier = Modifier) {
+fun RecommendListItem(
+    modifier: Modifier = Modifier,
+    recommend: Recommend
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = recommend.picture),
+            contentDescription = null,
+            modifier = Modifier.size(125.dp),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = stringResource(id = recommend.name),
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
 
+@Composable
+@Preview(showBackground = true)
+fun RecommendListPreview() {
+    MyPohangAppTheme {
+        RecommendListScreen(
+            recommendList = CategoryAndRecommendRepository.recommends,
+            onRecommendClick = {})
+    }
 }
