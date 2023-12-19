3fc75597bee19406427d78ac89dedb1499096ffd
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-19 14:46:05 +0900

    Create RecommendDetailScreen

---
+++ Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/ui/RecommendDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.example.mypohangapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RecommendDetailScreen(modifier: Modifier = Modifier) {
+
+}
