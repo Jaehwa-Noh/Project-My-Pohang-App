0261e282b17066251a33d80ac3f97f383aee3f5c
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-19 15:18:48 +0900

    Change composable function name to MyPohangApp

--- Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/MainActivity.kt
+++ Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.mypohangapp.ui.CategoryScreen
+import com.example.mypohangapp.ui.MyPohangApp
 import com.example.mypohangapp.ui.theme.MyPohangAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CategoryScreen()
+                    MyPohangApp()
                 }
             }
         }
