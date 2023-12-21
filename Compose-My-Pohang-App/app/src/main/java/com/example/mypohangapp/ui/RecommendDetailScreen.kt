0fd8b4e00d02332eb62c462f1b8669701c46acbe
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-21 17:41:43 +0900

    Add TestTag string.

--- Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/ui/RecommendDetailScreen.kt
+++ Compose-My-Pohang-App/app/src/main/java/com/example/mypohangapp/ui/RecommendDetailScreen.kt
@@ -15,10 +15,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mypohangapp.R
 import com.example.mypohangapp.data.CategoryAndRecommendRepository
 import com.example.mypohangapp.model.Recommend
 import com.example.mypohangapp.ui.theme.MyPohangAppTheme
@@ -43,7 +45,8 @@ fun RecommendDetailScreen(
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(1f),
+                    .weight(1f)
+                    .testTag(stringResource(R.string.test_tag_detail_picture)),
                 contentScale = ContentScale.Crop
             )
             RecommendDetailTextArea(
