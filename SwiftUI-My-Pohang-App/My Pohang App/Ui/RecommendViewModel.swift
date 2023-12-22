da7c2cb668ce505c571e917a90b2ed66464d2bc8
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-22 22:49:49 +0900

    Create RecommendViewModel.

---
+++ SwiftUI-My-Pohang-App/My Pohang App/Ui/RecommendViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  RecommendViewModel.swift
+//  My Pohang App
+//
+//  Created by Jaehwa Noh on 12/22/23.
+//
+
+import Foundation
+
+class RecommendViewModel {
+    var selectedCategory: Category?
+    var recommends: [Recommend] = []
+    var selectedRecommend: Recommend?
+    
+    func setSelectedCategory(category: Category) {
+        selectedCategory = category
+        recommends = getRecommendFromRepository(category: category)
+    }
+    
+    private func getRecommendFromRepository(category: Category) -> [Recommend] {
+        CategoryAndRecommendRepository().recommends.filter {
+            $0.categoryType == category.category
+        }
+    }
+}
