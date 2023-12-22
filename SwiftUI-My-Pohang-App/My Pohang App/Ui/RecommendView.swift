17e873f010939d82e33adbcbdfa319a950bc2017
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-22 21:29:57 +0900

    Create RecommendView.

---
+++ SwiftUI-My-Pohang-App/My Pohang App/Ui/RecommendView.swift
@@ -0,0 +1,22 @@
+//
+//  RecommendView.swift
+//  My Pohang App
+//
+//  Created by Jaehwa Noh on 12/22/23.
+//
+
+import SwiftUI
+
+struct RecommendView: View {
+    var category: Category
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendView(category: CategoryAndRecommendRepository().categories[0])
+}
