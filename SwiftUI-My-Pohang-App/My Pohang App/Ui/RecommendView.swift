409c359ab7956e8d24e81989a9d222a21ae0a2dc
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-22 22:50:26 +0900

    Create RecommendView.

--- SwiftUI-My-Pohang-App/My Pohang App/Ui/RecommendView.swift
+++ SwiftUI-My-Pohang-App/My Pohang App/Ui/RecommendView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct RecommendView: View {
-    var category: Category?
+    var category: Category
+    private var recommendViewModel: RecommendViewModel
     
-    
+    init(category: Category) {
+        self.category = category
+        self.recommendViewModel = RecommendViewModel()
+        recommendViewModel.setSelectedCategory(category: category)
+    }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(recommendViewModel.recommends) {
+                        recommend in
+                        RecommendListItem(recommend: recommend)
+                    }
+                }
+                .padding(8)
+            }
+            
+        }
+    }
+}
+
+struct RecommendListItem: View {
+    var recommend: Recommend
+    var body: some View {
+        HStack(alignment: .center, spacing: 8) {
+            Image(recommend.picture)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 125, height:125)
+                .clipped()
+            
+            Text("\(recommend.name)")
+                .font(.title)
+        }
     }
 }
 
