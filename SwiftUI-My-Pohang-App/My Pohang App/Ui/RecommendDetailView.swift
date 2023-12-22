0f546665108ec87ef582a1cf119b924bb2124659
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-22 22:53:47 +0900

    Create RecommendDetailView.

---
+++ SwiftUI-My-Pohang-App/My Pohang App/Ui/RecommendDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendDetailView.swift
+//  My Pohang App
+//
+//  Created by Jaehwa Noh on 12/22/23.
+//
+
+import SwiftUI
+
+struct RecommendDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendDetailView()
+}
