8505482086c03ebbe76aa7d1ab7d998468325b0e
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-23 17:17:25 +0900

    Create MyPohangAppViewCompactTest.

---
+++ SwiftUI-My-Pohang-App/My Pohang AppUITests/MyPohangAppViewCompactTest.swift
@@ -0,0 +1,59 @@
+//
+//  MyPohangAppViewTest.swift
+//  My Pohang AppUITests
+//
+//  Created by Jaehwa Noh on 12/23/23.
+//
+
+import XCTest
+
+final class MyPohangAppViewCompactTest: XCTestCase {
+
+    var app: XCUIApplication?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app?.launch()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        app = nil
+    }
+
+    func test_CompactDevice_CategoryView_VerifyContent() throws {
+        let categoryCoffeeButton = app!.buttons["카페"]
+        XCTAssertTrue(categoryCoffeeButton.exists)
+    }
+    
+    func test_CompactDevice_ClickCategory_ShowRecommendView() throws {
+        navigateRecommend()
+        let recommendButton = app!.buttons["러블랑"]
+        XCTAssertTrue(recommendButton.exists)
+    }
+    
+    func test_CompactDevice_ClickRecommend_ShowRecommendDetailView() throws {
+        navigateRecommendDetail()
+        let informationText = app!.staticTexts["Information"]
+        XCTAssertTrue(informationText.exists)
+    }
+    
+    private func navigateRecommend() {
+        let categoryCoffeeButton = app!.buttons["카페"]
+        categoryCoffeeButton.tap()
+    }
+    
+    private func navigateRecommendDetail() {
+        navigateRecommend()
+        let recommendButton = app!.buttons["러블랑"]
+        recommendButton.tap()
+    }
+}
