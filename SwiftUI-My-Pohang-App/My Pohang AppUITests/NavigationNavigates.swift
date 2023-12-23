d92e9b94f7a9cea7d7504e20177aef63378c0c9d
Jaehwa Noh <shwoghk14@gmail.com>
2023-12-23 19:46:42 +0900

    Create sidebar function.

--- SwiftUI-My-Pohang-App/My Pohang AppUITests/NavigationNavigates.swift
+++ SwiftUI-My-Pohang-App/My Pohang AppUITests/NavigationNavigates.swift
@@ -18,3 +18,9 @@ func navigateRecommendDetail(app: XCUIApplication) {
     let recommendButton = app.buttons["러블랑"]
     recommendButton.tap()
 }
+
+func openSidebarWithCount(app: XCUIApplication, count: Int) {
+    let toggleSidebarButton = app.buttons["ToggleSidebar"]
+    XCTAssertTrue(toggleSidebarButton.exists)
+    toggleSidebarButton.tap(withNumberOfTaps: count, numberOfTouches: 1)
+}
